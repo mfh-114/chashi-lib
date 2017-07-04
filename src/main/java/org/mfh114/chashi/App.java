412ca5426347b73d3a7cbedbe6e96fa3716ee32b
fhaider <firoj.haider@relais-intl.com>
2017-07-03 20:20:26 -0400

    initial project setup

---
+++ src/main/java/org/mfh114/chashi/App.java
@@ -0,0 +1,13 @@
+package org.mfh114.chashi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/org/mfh114/chashi/testng/AppTest.java
@@ -0,0 +1,14 @@
+package org.mfh114.chashi;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    public void test()
+    {
+    }
+
+}
