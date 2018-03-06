3cd7e844613289bf6dfa3d3753838758b75981e3
fhaider <firoj.haider@relais-intl.com>
2018-03-05 23:00:40 -0500

    Remove fsm.

--- src/main/java/org/mfh114/chashi/fsm/State.java
+++
@@ -1,9 +0,0 @@
-package org.mfh114.chashi.fsm;
-
-import org.mfh114.chashi.ChashiException;
-
-public interface State {
-	
-	public void execute() throws ChashiException;
-
-}
--- src/main/java/org/mfh114/chashi/fsm/TEngineErrorHandler.java
+++
@@ -1,7 +0,0 @@
-package org.mfh114.chashi.fsm;
-
-public interface TEngineErrorHandler {
-
-	public void handleErrorOnOccurred(Exception exception);
-
-}
